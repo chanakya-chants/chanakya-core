09b33bb8f9b398b6cad7c82586c9e8cf4cf3d95e
Suman Paul <sumank.paul@gmail.com>
2016-05-16 19:38:32 +0530

    remove Facebook server code and return the app

--- index.js
+++ index.js
@@ -163,6 +163,7 @@
     app.token = config.token;
     app.mount = config.mount;
     mount(app.mount);
+    return app;
   };
 
   core.getExpectation = function () {
@@ -171,65 +172,7 @@
 
   var mount = function (mountPoint) {
     var libs = require('require-all')(__dirname + '/../../' + mountPoint);
-  }
-
-  // server.set('port', (process.env.PORT || 3000));
-  //
-  // server.use(bodyParser.urlencoded({extended: false}));
-  // server.use(bodyParser.json());
-  // server.use('/img', express.static(__dirname + '/img'));
-  // // server.use('/', express.static(__dirname + '/public'));
-  //
-  // server.get('/', function (req, res) {
-  //   res.sendFile(path.join(__dirname, '../../public', 'index.html'));
-  // });
-  //
-  // server.get('/mi.png', function (req, res) {
-  //   res.sendFile(path.join(__dirname, '../../public', 'mi.png'));
-  // });
-  //
-  // server.get('/webhook', function (req, res) {
-  //   console.log('get webhook' + req.query['hub.verify_token']);
-  //   if (req.query['hub.verify_token'] === app.token) {
-  //     res.send(req.query['hub.challenge']);
-  //   } else {
-  //     res.send('Error, wrong validation token');
-  //   }
-  // });
-  //
-  // server.post('/webhook/', function (req, res) {
-  //
-  //   messaging_events = req.body.entry[0].messaging;
-  //
-  //   for (i = 0; i < messaging_events.length; i++) {
-  //     var event = req.body.entry[0].messaging[i];
-  //     var sender = event.sender.id;
-  //
-  //     console.log(event, sender);
-  //
-  //     if (_.isUndefined(chatSession[sender])) {
-  //       https.get('https://graph.facebook.com/v2.6/' + sender + '?access_token=' + app.token, function (res) {
-  //         res.setEncoding('utf8');
-  //         res.on('data', function (d) {
-  //           d = JSON.parse(d);
-  //           d.id = sender;
-  //           d.expectation = app.expectation;
-  //           chatSession[sender] = _.clone(d);
-  //           handleMessage(event, chatSession[sender]);
-  //         });
-  //       }).on('error', function (e) {
-  //         console.error(e);
-  //       });
-  //     } else {
-  //       handleMessage(event, chatSession[sender]);
-  //     }
-  //   }
-  //   res.sendStatus(200);
-  // });
-  //
-  // server.listen(server.get('port'), function () {
-  //   console.log('Node server is running on port', server.get('port'));
-  // });
+  };
 
   core.handleMessage = function (event, sender) {
     if (event.message && event.message.text) {
