255485971ed746ce2f0247620a2274de5fb95c63
Suman Paul <sumank.paul@gmail.com>
2016-05-20 13:53:10 +0530

    # If applied, this commit will... add ~50% test coverage
    # Explain why this change is being made
    improve test coverage and code quality
    
    # Provide links to any relevant tickets, articles or other resources

--- tests/core.test.js
+++ tests/core.test.js
@@ -1,29 +1,93 @@
 var chai = require('chai');
 var expect = chai.expect;
-var core = require('./../index.js');
+var sinon = require('sinon');
+var rewire = require('rewire');
+var core = rewire('./../index.js');
 
 describe('register artifact', function () {
   it('should register a response', function () {
-    core.response('success', function () {
-      return 'success';
+    core.expectation('greetings', ['isGreetings'], function (res) {
+      switch (res) {
+        case true:
+          return {
+            data: res,
+            responses: ['fail', 'success']
+          };
+          break;
+        case false:
+          return ['fail'];
+          break;
+      }
     });
-    expect(core.getAllResponses().length).to.equal(1);
+    expect(core.getAllExpectations().length).to.equal(1);
+  });
+  it('should not re-register a response but throw an error', function () {
+    expect(() => core.expectation('greetings', ['isGreetings'], function (res) {
+      switch (res) {
+        case true:
+          return {
+            data: res,
+            responses: ['fail', 'success']
+          };
+          break;
+        case false:
+          return ['fail'];
+          break;
+      }
+    })).to.throw('expectations : greetings already registered');
   });
   it('should register a validator', function () {
-    core.validator('isGreetings', function (message) {
-      return message === 'hi';
+    core.validator('isGreetings', null, function (message) {
+      return Q.fcall(function () {
+        return message == 'hi';
+      });
     });
     expect(core.getAllValidators().length).to.equal(1);
   });
-
   it('should register a expectation', function () {
-    core.expectation('phoneno', function () {
+    core.response('fail', 'greetings', function (to, validatorResult) {
       return {
-        validators: ['isPhoneno'],
-        success: ['success'],
-        fail: ['fail']
+        text: `I am sorry ${to.first_name}, I am unable to understand what you mean. ${validatorResult}`
       };
-    }, 'greetings');
-    expect(core.getAllExpectations().length).to.equal(1);
+    });
+    expect(core.getAllResponses().length).to.equal(1);
+  });
+});
+
+describe('app bootstrap', function () {
+  it('should bootstrap the app and return a valid app', function () {
+
+    sinon.stub(core, 'mount', function () {
+    });
+    expect(core.bootstrap({
+      mount: 'bot',
+      expectation: 'greetings',
+      token: 'token'
+    })).to.eql(
+      {
+        mount: 'bot',
+        expectation: 'greetings',
+        token: 'token'
+      }
+    );
+  });
+});
+
+describe('session management', function () {
+  it('should set session', function () {
+    core.setSession({id: 1234});
+    expect(core.getSession('1234')).to.eql({id: 1234});
+  });
+});
+
+describe('expectations', function () {
+  it('shouls return the app current expectation', function () {
+    core.bootstrap({
+      mount: 'bot',
+      expectation: 'greetings',
+      token: 'token'
+    });
+
+    expect(core.getExpectation()).to.equal('greetings');
   })
 });
